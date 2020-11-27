39a12a5d90478cb2f3a6d517ae0467454d03d662
werg1 <werg1@DESKTOP-9B10C7H>
2020-11-27 10:26:11 +0900

    객체지향 연습문제

---
+++ src/com/javaex/ex03/SongApp.java
@@ -0,0 +1,18 @@
+package com.javaex.ex03;
+
+public class SongApp {
+
+	public static void main(String[] args){
+		
+		Song s1 = new Song("좋은날", "아이유", "Real", "이민수", "2010", 3);
+		
+		//출력결과를 예상해서 주석으로 작성해 보세요
+		
+		//파라미터 6개가 출력된다
+		
+		
+	}
+
+}
+
+
